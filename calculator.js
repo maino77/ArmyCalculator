97474676e16b7623d8790961617a3877fcadcfe2
ParkJongJoon <whdwnsdk8111@naver.com>
2021-08-05 21:45:26 +0900

    Fifth Commit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
 var armyBtn = document.getElementsByClassName("armyBtn");
 var selectDateBtn = document.getElementsByClassName("selectDateBtn");
-
 function handleClick(event) {
 
 
@@ -18,16 +17,21 @@ function init() {
     for (let i = 0; i < armyBtn.length; i++) {
         armyBtn[i].addEventListener("click", handleClick);
     }
+    for(let i=0; i< armyBtn.length; i++){
+        if(armyBtn[i] === armyBtn[i]){
+            
+        }
+    }
 }
 
 init();
 
 
 
-var 육군 = "18개월"
-var 해군 = "20개월"
-var 공군 = "22개월"
-var 해병대 = "18개월"
-var 사회복무요원 = "21개월"
-var 의경해경 = "18개월"
-var 소방원 = "20개월"
+// var 육군 = "18개월 = 1 year 6 month"
+// var 해군 = "20개월 = 1 year 8 month"
+// var 공군 = "22개월 = 1 year 10 month"
+// var 해병대 = "18개월 = 1 year 6 month"
+// var 사회복무요원 = "21개월 = 1 year 9 month"
+// var 의경해경 = "18개월 = 1 year 6 month"
+// var 소방원 = "20개월 = 1 year 8 month"
