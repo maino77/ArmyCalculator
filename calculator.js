4ee8a78a30b1e5ca3a47ad8a46d6e3cb44ed5b4a
ParkJongJoon <whdwnsdk8111@naver.com>
2021-08-10 23:44:59 +0900

    Seventh Commit

--- calculator.js
+++ calculator.js
@@ -2,25 +2,24 @@ var armyBtn = document.getElementsByClassName("armyBtn");
 var selectDateBtn = document.getElementsByClassName("selectDateBtn");
 function handleClick(event) {
 
-
     if (event.target.classList[1] === "clicked") {
         event.target.classList.remove("clicked");
+        armyBtn.style.backgroundColor = "#E2E2E2";
+        armyBtn.style.color = "black";
     } else {
         for (let i = 0; i < armyBtn.length; i++) {
             armyBtn[i].classList.remove("clicked");
         }
     }
+
     event.target.classList.add("clicked");
 }
+
 
 function init() {
     for (let i = 0; i < armyBtn.length; i++) {
         armyBtn[i].addEventListener("click", handleClick);
-    }
-    for(let i=0; i< armyBtn.length; i++){
-        if(armyBtn[i] === armyBtn[i]){
-            
-        }
+
     }
 }
 
